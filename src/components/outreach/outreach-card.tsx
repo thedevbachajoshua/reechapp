24a9b9da0f31442373b3ce58dd0b4f53965e7b09
Joshua Bacha <joshbach1234@gmail.com>
2025-11-04 17:35:42 +0000

    in the outreaches tab each card should slightly float up or animate it b

--- src/components/outreach/outreach-card.tsx
+++ src/components/outreach/outreach-card.tsx
@@ -31,7 +31,7 @@ export function OutreachCard({ event }: { event: OutreachEvent }) {
   };
 
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-xl">
       <CardHeader>
         <div className="flex items-start justify-between">
             <CardTitle className="text-xl">{event.title}</CardTitle>
