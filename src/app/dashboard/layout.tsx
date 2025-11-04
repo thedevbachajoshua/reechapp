50ed5dbdf3daf713fa6202ecc7290b4ab39d79c6
Joshua Bacha <joshbach1234@gmail.com>
2025-11-04 18:14:12 +0000

    Try fixing this error: `Runtime Error: ReferenceError: React is not defi

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import AppSidebar from '@/components/layout/app-sidebar';
 import Header from '@/components/layout/header';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
