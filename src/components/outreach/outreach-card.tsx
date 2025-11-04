e8d45b5c731cef4c2e7cc191984f606806126504
Joshua Bacha <joshbach1234@gmail.com>
2025-11-04 17:16:51 +0000

    now add some confinement to the various outreaches and their information

--- src/components/outreach/outreach-card.tsx
+++ src/components/outreach/outreach-card.tsx
@@ -31,7 +31,7 @@ export function OutreachCard({ event }: { event: OutreachEvent }) {
   };
 
   return (
-    <Card className="flex flex-col">
+    <Card className="flex flex-col border">
       <CardHeader>
         <div className="flex items-start justify-between">
             <CardTitle className="text-xl">{event.title}</CardTitle>
