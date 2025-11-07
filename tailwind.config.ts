715ac28457c4f0d874a68a10ef10235c54adf06d
Joshua Bacha <joshbach1234@gmail.com>
2025-11-07 12:02:28 +0000

    use quintissetial by astigmatic from goggle font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Dancing Script"', 'cursive'],
+        headline: ['"Quintessential"', 'cursive'],
         code: ['monospace'],
       },
       colors: {
